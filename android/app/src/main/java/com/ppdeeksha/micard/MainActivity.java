05238ccc695b6f035ca7c08273f7521226b60eed
deeks-codestar <deekshustle@gmail.com>
2020-04-18 14:32:40 -0700

    First commit

---
+++ android/app/src/main/java/com/ppdeeksha/micard/MainActivity.java
@@ -0,0 +1,6 @@
+package com.ppdeeksha.micard;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        body: Column(
+          children: <Widget>[
+            CircleAvatar(
+              radius: 50,
+              backgroundImage: AssetImage('images/nakul.jpg'),
+            ),
+            Text(
+                "Nakul Rajan",
+                style: TextStyle(
+                  fontSize: 40,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'Pacifico',
+                )
+            ),
+            Text(
+                "Software Developer",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontFamily: 'SourceSansPro',
+                  fontSize: 15,
+                  fontWeight: FontWeight.bold,
+                )
+            ),
+
+          ],
+        ),
+      ),
+    );
+  }
+}
